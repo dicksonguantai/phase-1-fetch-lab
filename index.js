3a5347ee2583fa8bbc6c4f9fefe8a16fb04c608c
dicksonmutwiriguantai <dickson.mutwiri@student.moringaschool.com>
2023-04-16 19:35:05 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
   
+   return fetch("https://anapioficeandfire.com/api/books",)
+    .then(res => res.json)
+    .then(books=> {
+      console.log(books)
+      renderBooks(Object.values(books))// turns the object into an array that is iteratble by the for each loop in renderBooks Function 
+
+    })
 }
 
 function renderBooks(books) {
